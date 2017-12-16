5d17d5a91d06615ceb1301210e212d5ff7d8a99d
Isaac Maria <isaacmaria2@yahoo.com>
2017-12-16 12:23:55 +0530

    progress 2.51

--- app/src/main/java/com/github/ivanmaria/attendanceassistant/StatsFragment.java
+++ app/src/main/java/com/github/ivanmaria/attendanceassistant/StatsFragment.java
@@ -1,18 +1,14 @@
 package com.github.ivanmaria.attendanceassistant;
 
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.ListView;
 
 
 /**
@@ -22,20 +18,6 @@ public class StatsFragment extends Fragment {
     private TabLayout tabLayout;
     private ViewPager viewPager;
 
-    /*
-    ListView lv, lvp;
-    Context context;
-    SwipeRefreshLayout mSwipeRefreshLayout;
-    public int subCount=4, pracCount=5;
-    float height;
-    public static String [] subList={"CN","OS","MP","SOOAD"};
-    public static String [] hours={"18/25","16/18","13/20","17/18"};
-    public static int [] percent={75,80,65,95};
-
-    public static String [] subListP={"CN","OS","MP","SOOAD","WT"};
-    public static String [] hoursP={"4/8","8/10","7/10","3/10","10/10"};
-    public static int [] percentP={50,80,70,30,100};
-*/
     public StatsFragment() {
         // Required empty public constructor
     }
@@ -47,35 +29,11 @@ public class StatsFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_stats, container, false);
 
-        tabLayout = (TabLayout)v.findViewById(R.id.tabs_stats);
-        viewPager = (ViewPager)v.findViewById(R.id.view_pager_stats);
+        tabLayout = v.findViewById(R.id.tabs_stats);
+        viewPager = v.findViewById(R.id.view_pager_stats);
         viewPager.setAdapter(new StatsFragmentPageAdapter(getChildFragmentManager()));
         tabLayout.setupWithViewPager(viewPager);
-        /*
-        context=getContext();
-        MainActivity main = (MainActivity) getActivity();
-        lv=(ListView) v.findViewById(R.id.listView);
-        height = subCount * 50 * getContext().getResources().getDisplayMetrics().density;
-        lv.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, (int)height));
-        lvp=(ListView) v.findViewById(R.id.ListViewP);
-        height = pracCount * 50 * getContext().getResources().getDisplayMetrics().density;
-        lvp.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, (int)height));
-        lv.setAdapter(new CustomAdapter(main, subList,hours,percent));
-        lvp.setAdapter(new CustomAdapter(main, subListP,hoursP,percentP));
-        mSwipeRefreshLayout = (SwipeRefreshLayout) v.findViewById(R.id.swipeToRefresh);
 
-
-
-
-
-        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                mSwipeRefreshLayout.setRefreshing(false);
-            }
-        });
-
-*/
     return v;
     }
     @Override
